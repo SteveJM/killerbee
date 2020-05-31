81c23c95596f4a6c0a187334cdd1dedf16751aa2
SteveJM <stephen.martin@royalnavymail.mod.uk>
2020-05-31 15:44:12 +0100

    Rebase Raspbee to py3 branch.

--- killerbee/dev_rzusbstick.py
+++ killerbee/dev_rzusbstick.py
@@ -525,7 +525,9 @@ class RZUSBSTICK:
         for pnum in range(count):
             # Format for packet is opcode RZ_CMD_INJECT_FRAME, one-byte length, 
             # packet data
-            self.__usb_write(RZ_USB_COMMAND_EP, struct.pack("BB", RZ_CMD_INJECT_FRAME, len(packet)) + packet)
+            # self.__usb_write(RZ_USB_COMMAND_EP, [RZ_CMD_INJECT_FRAME, len(packet)] + packet)
+
+            self.__usb_write(RZ_USB_COMMAND_EP, [RZ_CMD_INJECT_FRAME, len(packet)] + list(bytes(packet, 'latin-1')))
             time.sleep(delay)
 
     # KillerBee expects the driver to implement this function
@@ -603,7 +605,7 @@ class RZUSBSTICK:
                 # The last byte of frame data is the link quality indicator
                 ret['lqi'] = framedata[-1]
                 # Convert the framedata to a string for the return value
-                ret[0] = ''.join(framedata[:-1])
+                ret[0] = b''.join(framedata[:-1]).decode('latin-1')
                 ret['bytes'] = ret[0]
                 return ret
             else:
